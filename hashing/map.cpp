f41282f1fa653b8596df91e42aa787f684f98e22
PratyushSharma <psvt2007@gmail.com>
2026-04-30 23:55:42 +0530

    Hashing

--- hashing/map.cpp
+++ hashing/map.cpp
@@ -1,5 +1,7 @@
 #include<iostream>
 #include <map>
+
+#include <unordered_map>
 using namespace std;
 int main(){
 int n;
@@ -8,14 +10,20 @@ int arr[n];
 for(int i =0;i<n;i++){
     cin>>arr[i];
 }
-map<int,int>mpp;
+unordered_map<int,int>mpp;
 for(int i =0;i<n;i++){
     mpp[arr[i]]++;
 }
-
+//we should use unordermap first if there is error of time limit exceed we should go to map as unordermap gives you least time complexity in worst case in collision only it will give you n square time complexity other wise it will give you big o of n time complextiy 
 
 int q ;
 cin>>q;
+while(q--){
+    int number;
+    cin>>number;
+    cout<<mpp[number]<<endl;
+    
+}
 while(q--){
     int number;
     cin>>number;
